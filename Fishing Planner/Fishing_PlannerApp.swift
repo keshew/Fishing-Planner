4f1d05d543ea712dd2731c9a6b601a4492b38d93
keshew <artyom.korotckov2017@yandex.ru>
2026-01-14 17:40:01 +0500

    Initial Commit

---
+++ Fishing Planner/Fishing_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fishing_PlannerApp.swift
+//  Fishing Planner
+//
+//  Created by Артём Коротков on 14.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct Fishing_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
